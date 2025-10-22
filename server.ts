d56219d2ca3c1a040452b18b35ff996623dbce50
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2025-10-22 12:01:16 +0100

    Fix

--- server.ts
+++ server.ts
@@ -1,36 +1,39 @@
-import { Glob } from 'bun';
+import { Glob } from "bun";
 
 const CONSTANTS = {
-    SERVER_PATH: "./dist/server/server.js",
-    CLIENT_PATH: "./dist/client",
-}
+	SERVER_PATH: "./dist/server/server.js",
+	CLIENT_PATH: "./dist/client",
+};
 
 type ServerModule = {
-    default: {
-        fetch: (req: Request) => Promise<Response>;
-    }
-}
-
-const serverModule = await import(CONSTANTS.SERVER_PATH) as ServerModule;
+	default: {
+		fetch: (req: Request) => Promise<Response>;
+	};
+};
 
 async function getStaticRoutes(): Promise<Record<string, () => Response>> {
-    const paths = await Array.fromAsync(new Glob(`${CONSTANTS.CLIENT_PATH}/**/*`).scan("."));
-    return Object.fromEntries(
-        paths.map((path) => {
-            const file = Bun.file(path);
-            return [
-                path.replace(CONSTANTS.CLIENT_PATH, ""),
-                () => new Response(file, { headers: { "Content-Type": file.type } })
-            ];
-        })
-    ) as Record<string, () => Response>;
+	const paths = await Array.fromAsync(
+		new Glob(`${CONSTANTS.CLIENT_PATH}/**/*`).scan("."),
+	);
+	return Object.fromEntries(
+		paths.map((path) => {
+			const file = Bun.file(path);
+			return [
+				path.replace(CONSTANTS.CLIENT_PATH, ""),
+				() => new Response(file, { headers: { "Content-Type": file.type } }),
+			];
+		}),
+	) as Record<string, () => Response>;
 }
 
 Bun.serve({
-    port: process.env.PORT ?? 3000,
-    routes:{
-        ...(await getStaticRoutes()),
-    "/*": async (req) => {
-        return serverModule.default.fetch(req);
-    }}
-})
+	port: process.env.PORT ?? 3000,
+	routes: {
+		...(await getStaticRoutes()),
+		"/*": async (req) => {
+            const serverModule = (await import(CONSTANTS.SERVER_PATH)) as ServerModule;
+            
+			return serverModule.default.fetch(req);
+		},
+	},
+});
